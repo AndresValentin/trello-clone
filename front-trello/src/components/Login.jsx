8ed4b8e16331d61e4db2c37ef13907700bf6eab1
Valentin Navarro <valentinandress@gmail.com>
2025-09-01 15:59:33 -0400

    Login

---
+++ front-trello/src/components/Login.jsx
@@ -0,0 +1,12 @@
+    export default function Login() {
+  return (
+    <div>
+      <h2>Pantalla de Login</h2>
+      <form>
+        <input type="text" placeholder="Usuario" />
+        <input type="password" placeholder="Contraseña" />
+        <button type="submit">Ingresar</button>
+      </form>
+    </div>
+  );
+}
