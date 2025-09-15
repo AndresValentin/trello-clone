b2b86c02ee9e0ed9aa9f0eb0576423894014696d
Valentin Navarro <valentinandress@gmail.com>
2025-09-15 16:00:48 -0300

    Login pruebas

--- front-trello/src/components/Login.jsx
+++ front-trello/src/components/Login.jsx
@@ -1,12 +1,19 @@
-    export default function Login() {
-  return (
-    <div>
-      <h2>Pantalla de Login</h2>
-      <form>
-        <input type="text" placeholder="Usuario" />
-        <input type="password" placeholder="Contraseña" />
-        <button type="submit">Ingresar</button>
-      </form>
-    </div>
-  );
-}
+import { useState } from "react";
+
+
+
+
+function Login() {
+
+    const [name,setName] = useState("esteban");
+
+    return(
+        <div>
+        <h1>Pantalla de Login</h1>
+        <h2>Nombre: {name}</h2>
+        <input type="text" placeholder="Ingrese su nombre"></input>
+        <button onClick={() => alert(`Hola, ${name}`)}>Enviar</button>
+        </div>
+    )
+ }
+ export default Login;
