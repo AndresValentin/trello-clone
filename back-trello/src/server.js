f43427636471b04c8baf07561c02831092f0c9af
Valentin Navarro <valentinandress@gmail.com>
2025-09-18 12:12:38 -0300

    cambios 2

---
+++ back-trello/src/server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+
+//datos de prueba para la api
+let users = [
+    {id:1,name:'usuario 1'},
+    {id:2,name:'usuario 2'}
+]
+
+//crear servidor 
+const server = http.createServer((req))
