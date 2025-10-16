721057dad818fb43d65afbb37d1a6a7f1803f8cf
Valentin Navarro <valentinandress@gmail.com>
2025-10-16 16:42:25 -0300

    olvide contraseña

--- front-trello/src/components/popup.jsx
+++ front-trello/src/components/popup.jsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
-
-function Popup(){
+import { useState,useEffect } from "react";
 
 
+function Popup({isOpen,onClose,onSubmit}){
+
+    const[email,setEmail] = useState("");
+
+    useEffect(() => {
+        if (!isOpen) return;
+    });
+
+
+    if (!isOpen) return null;
+
+
+
+    return (
+        <div className="modal-pass">
+            <h3>Recuperar contraseña</h3>
+            <form>
+                <label htmlFor="forgot-email">correo</label>
+                <input
+                    id="forgot-email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail>(e.target.value)}
+                    placeholder="email"
+                    required
+                >
+                </input>
+                <button type="button">Cancelar</button>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    )
 }
 
 export default Popup
